c8f1591da4124aafe1363ae568c7b8e5cc785905
Luke Tyson <ltyson@amplience.com>
2021-08-31 13:30:32 +0100

    Gatsby components and styling

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+export default function about() {
+  return (
+    <main>
+      <Layout pageTitle="About Me">
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    </Layout>
+    </main>
+  )
+}
